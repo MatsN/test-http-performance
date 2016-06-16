420027b96c370f69366e4968938a7938f005fc31
MatsN <xmatsn@hotmail.com>
2016-06-16 21:50:39 +0200

    added two arguments for how the client will behave

--- client.js
+++ client.js
@@ -1,9 +1,12 @@
-var client = function(name, headers) {
+var client = function(name, headers, times_to_run, msec_between_requests) {
     var self = this;
-    this.name = name;
-    this.headers = headers;
+    self.name = name;
+    self.headers = headers;
+    self.times_to_run = times_to_run;
+    self.msec_between_requests = msec_between_requests;
+
 }
 
-module.exports = function (name, headers) {
-    return new client(name, headers);
+module.exports = function (name, headers, times_to_run, msec_between_requests) {
+    return new client(name, headers, times_to_run, msec_between_requests);
 }
