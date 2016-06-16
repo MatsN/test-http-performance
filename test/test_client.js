b00edfe86fbe9de96669665c043621d526fa3dc0
MatsN <xmatsn@hotmail.com>
2016-06-16 21:52:03 +0200

    extended tests to include new attributes

--- test/test_client.js
+++ test/test_client.js
@@ -11,9 +11,13 @@ describe('client', function () {
         it('object should have all attributes that was given to the constructor', function(done) {
             var name = 'testclient';
             var headers = { 'testheader': 'abc'};
-            var testclient = require('../client.js')(name,headers);
+            var times_to_run = 10;
+            var msec_between_requests = 1000;
+            var testclient = require('../client.js')(name,headers,times_to_run,msec_between_requests);
             assert.equal(testclient.name, name);
-            assert.equal(testclient.headers, headers);            
+            assert.equal(testclient.headers, headers);
+            assert.equal(testclient.times_to_run, times_to_run);
+            assert.equal(testclient.msec_between_requests,msec_between_requests);
             done();
         });
     });
